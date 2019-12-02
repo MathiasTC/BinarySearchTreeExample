bc89ac4a5d67e58bd3913d544c0413570f4c69a2
Mathias Tøgersen Christensen <mtc@skov.dk>
2019-12-02 12:30:52 +0100

    Add project files.

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        static Register reg = new Register();
+        static SortedList neww = new SortedList();
+        static void Main(string[] args)
+        {
+            //AddToList();
+            Console.WriteLine("Object oriented under here");
+            reg.SetNames(neww);
+            AddToListObjectWay();
+            Console.ReadKey();
+        }
+        static public void AddToListObjectWay()
+        {         
+            IList myKeyList = neww.GetKeyList();
+            IList myValueList = neww.GetValueList();
+            foreach (var item in neww)
+            {
+                Console.WriteLine($"{myKeyList} -> {myValueList}");
+            }
+        }
+        static public void AddToList()
+        {
+            SortedList stlRoot = new SortedList();
+            Random rng = new Random();
+            int[] rngList = new int[7];
+            for (int i = 0; i < rngList.Length; i++)
+            {
+                rngList[i] = rng.Next(0, 50);
+            }            
+            stlRoot.Add("Bookfrau", rngList[0]);
+            stlRoot.Add("Fraubook", rngList[1]);
+            stlRoot.Add("Neinnein", rngList[2]);
+            stlRoot.Add("Dragon eh", rngList[3]);
+            stlRoot.Add("Oi mateuu", rngList[4]);
+            stlRoot.Add("All here", rngList[5]);
+            stlRoot.Add("Cyka bly", rngList[6]);            
+
+            IList myKeyList = stlRoot.GetKeyList();
+            IList myValueList = stlRoot.GetValueList();
+
+            Console.WriteLine("WORD\t\tPAGE");
+            for (int i = 0; i < stlRoot.Count; i++)
+            {                
+                Console.WriteLine("{0}    -    {1}", myKeyList[i], myValueList[i]);                
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Register.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Register 
+    {
+        public Keyword Key { get; set; }
+        public SortedList MySortedList { get; set; }     
+        public void SetNames(SortedList sort)            
+        {
+            sort.Add("hello", new SortedSet<int>() { 2, 3, 5, 8 });
+            sort.Add("ualsa", new SortedSet<int>() { 1, 50, 115, 612 });
+            sort.Add("bliea", new SortedSet<int>() { 41, 212, 356, 709 });
+            sort.Add("asdde", new SortedSet<int>() { 409, 800 });
+            sort.Add("lkoie", new SortedSet<int>() { 2, 1002 });
+            sort.Add("popiu", new SortedSet<int>() { 4000 });
+            sort.Add("nneje", new SortedSet<int>() { 2 });
+        }
+    }
+    class Keyword
+    {
+        public string Name { get; set; }
+    }
+}
